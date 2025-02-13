eb4646f51e93f99d68e5ac13d9a5a93559dfa340
Balaji E <124427573+CodeByBalajiE@users.noreply.github.com>
2025-02-13 23:13:11 +0530

    Create poww.java

---
+++ poww.java
@@ -0,0 +1,11 @@
+public class poww {
+    public static void main(String[] args) {
+        int a=2,b=5;
+        int res=1;
+        for (int i=0;i<b;i++){
+            res*=a;
+        }
+        System.out.println(res);
+    }
+
+}
